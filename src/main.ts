0f8211082a6dd68282868ed576bec3c3146cda1b
avnerjose <avnerj.g.r@gmail.com>
2022-11-04 12:09:36 -0300

    Atualizando arquivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,36 @@ import { MergeSort } from "./classes/MergeSort";
 import { QuickSort } from "./classes/QuickSort";
 import { SelectionSort } from "./classes/SelectionSort";
 
+function shuffle(array: number[]) {
+  let currentIndex = array.length,
+    randomIndex: number;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
 let sorting: DataSorting<number>;
 let arrayToSort = [10, 2, 5, 0, 6, 20, 14];
 let result: number[] = [];
 
 sorting = new BubbleSort();
+shuffle(arrayToSort);
 result = sorting.getSortingAlgorithm().sort(arrayToSort);
 
 console.log("Bubble Sort");
 console.log(result);
 
 sorting = new QuickSort();
+shuffle(arrayToSort);
 result = sorting
   .getSortingAlgorithm()
   .sort(arrayToSort, 0, arrayToSort.length - 1);
@@ -23,6 +42,7 @@ console.log("Quick Sort");
 console.log(result);
 
 sorting = new MergeSort();
+shuffle(arrayToSort);
 result = sorting
   .getSortingAlgorithm()
   .sort(arrayToSort, 0, arrayToSort.length - 1);
@@ -31,6 +51,7 @@ console.log("Merge Sort");
 console.log(result);
 
 sorting = new SelectionSort();
+shuffle(arrayToSort);
 result = sorting.getSortingAlgorithm().sort(arrayToSort);
 
 console.log("Selection Sort");
